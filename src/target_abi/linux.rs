f092ee449d70e93a9acecf742a488692a89f2be8
Hannes Karppila <hannes.karppila@gmail.com>
2019-08-21 16:53:07 +0300

    Fix exit function on Linux

--- src/target_abi/linux.rs
+++ src/target_abi/linux.rs
@@ -39,7 +39,7 @@ impl Operations for Interface {
                 io: true,
             }),
             MovImm(Register64::rdi, 0),
-            NamedBlackBox("exit".to_owned(), "exit".to_owned(), Effects {
+            NamedBlackBox("exit".to_owned(), "call exit".to_owned(), Effects {
                 flags: true,
                 registers: true,
                 control_flow: true,
